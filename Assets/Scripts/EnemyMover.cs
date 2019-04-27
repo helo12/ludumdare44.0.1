e2656b3ce380f318f4ce1c7d96a4d61d214d373e
Tim Allen <carver0428@hotmail.com>
2019-04-27 14:53:56 -0700

    started on enemy movement and detection working on health next.

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
+
+	[SerializeField]
+	private List<Transform> path = new List<Transform>();
+	[SerializeField]
+	private float movespeed = 2f;
+	[SerializeField]
+	private bool looping = false;
+	private int index = 0;
     // Start is called before the first frame update
     void Start()
     {
+		transform.position = path[index].transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		Move();
     }
+	private void Move()
+	{
+		while (index != path.Count)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, path[index].transform.position, movespeed * Time.deltaTime);
+
+			if(transform.position == path[index].transform.position)
+			{
+				index++;
+			}
+			else if (index == path.Count && looping == true) {
+				index = 0;
+			}
+		}
+	}
 }
+
+
